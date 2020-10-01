43c95ac053cae3fb22119120a44f05902af813a9
TookTalk2Mac <wootaejung24@gmail.com>
2020-10-01 18:18:33 +0900

    Refactor  html, css, js of nav
    좋은 보기를 위해 init() 내부의 함수를 밖으로 위치
    보이는 구성과 태그의 구성이  달라  html 태그를 새롭게 변경
    html태그에 맞게 css도 수정
    볼이 회전하는 효과를 사용하지 않기 때문에 handleScrolling을 삭제

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+'use strict';
 // Setting
 const DURATION_LONG = 1000,
   DURATION_MEDIUM = 800,
@@ -7,112 +8,95 @@ const DURATION_LONG = 1000,
   DURATION_SLIDE = 1300;
 
 const RESPONSE_WIDTH = 768,
-  MAP_WIDTH = 500;
-MAP_HEIGHT = 400;
+  MAP_WIDTH = 500,
+  MAP_HEIGHT = 400;
 
 // Navigation
 
-const pickers = document.querySelectorAll('.nav__picker'),
-  headers = document.querySelectorAll('.nav__header'),
-  nav = document.querySelector('.nav'),
-  sections = document.querySelectorAll('section'),
-  back = document.querySelector('.back'),
-  wave = document.querySelector('.nav__wave');
+const pickers = document.querySelectorAll('.nav__picker');
+const navSections = document.querySelectorAll('.nav__section');
 
+const nav = document.querySelector('.nav');
+const sections = document.querySelectorAll('section');
+const back = document.querySelector('.back');
+const wave = document.querySelector('.nav__wave');
+const navContent = document.querySelector('.nav__content');
 let state;
 
-function handlePick(event) {
-  event.preventDefault();
+pickers.forEach((picker) => {
+  picker.addEventListener('click', (event) => {
+    event.preventDefault();
+    state = event.target.dataset.id || event.target.parentNode.dataset.id;
+    console.log(state);
 
-  state = event.target.dataset.id;
-  const target = document.querySelector(`.${event.target.dataset.id}`);
+    const target = document.querySelector(`.${state}`);
 
-  sections.forEach((section) => {
-    if (section !== target) {
-      section.style.display = 'none';
-    } else {
-      section.style.display = 'block';
-    }
+    console.log(target);
+    // 클릭한 section을 표시함
+    sections.forEach((section) => {
+      if (section !== target) {
+        section.style.display = 'none';
+      } else {
+        section.style.display = 'block';
+      }
+    });
+    console.log(wave.classList);
+    // 물결효과 시작
+    wave.classList.remove('down_100vh');
+    console.log(wave.classList);
+    setTimeout(() => {
+      navContent.classList.add('up_150vh');
+    }, DURATION_MICRO);
+    // 시간을 두고
+    setTimeout(() => {
+      navSections.forEach((navSection) => {
+        navSection.classList.add('up_150vh');
+      });
+    }, DURATION_SHORT);
+
+    // 시간을 두고 wave 올리면서 section 자체를 올림
+    setTimeout(function () {
+      wave.classList.add('up_100vh');
+      sections.forEach((section) => {
+        section.classList.add('up_100vh');
+      });
+    }, DURATION_LONG);
+    // back버튼을 활성화 함
+    back.classList.add('back--active');
+    // nav의 zIndex는 내림
+    nav.style.zIndex = '-1';
   });
+});
 
-  wave.classList.remove('down_100vh');
-
+function handleBack(event) {
+  event.preventDefault();
+  back.classList.remove('back--active');
+  wave.classList.remove('up_100vh');
   setTimeout(function () {
-    headers.forEach((header) => {
-      header.classList.add('up_150vh');
+    sections.forEach((section) => {
+      section.classList.remove('up_100vh');
+    });
+  }, DURATION_MICRO);
+
+  setTimeout(() => {
+    navSections.forEach((navSection) => {
+      navSection.classList.remove('up_150vh');
     });
   }, DURATION_SHORT);
 
   setTimeout(function () {
-    pickers.forEach((picker) => {
-      picker.classList.add('up_150vh');
-    });
-  }, DURATION_REGULAR);
-
-  setTimeout(function () {
-    wave.classList.add('up_100vh');
-    sections.forEach((section) => {
-      section.classList.add('up_100vh');
-    });
+    wave.classList.add('down_100vh');
   }, DURATION_MEDIUM);
 
-  back.classList.add('back--active');
-
-  nav.style.zIndex = '-1';
-}
-
-function handleBack(event) {
-  event.preventDefault();
-  const target = document.querySelector(`.${state}`);
+  setTimeout(() => {
+    navContent.classList.remove('up_150vh');
+  }, DURATION_LONG);
 
   sections.forEach((section) => {
     section.style.display = 'none';
   });
 
   nav.style.zIndex = '0';
-  wave.classList.remove('up_100vh');
-
-  setTimeout(function () {
-    pickers.forEach((picker) => {
-      picker.classList.remove('up_150vh');
-    });
-  }, DURATION_SHORT);
-
-  setTimeout(function () {
-    headers.forEach((header) => {
-      header.classList.remove('up_150vh');
-    });
-  }, DURATION_REGULAR);
-
-  setTimeout(function () {
-    wave.classList.add('down_100vh');
-    sections.forEach((section) => {
-      section.classList.remove('up_100vh');
-    });
-  }, DURATION_MEDIUM);
-
-  back.classList.remove('back--active');
 }
 
-// About > Rotation on Scrolling
-
-const balls = document.querySelectorAll('.container>img');
-
-function handleScrolling(event) {
-  event.preventDefault();
-  balls.forEach((ball) => {
-    ball.style.transform = `rotate(${window.pageYOffset / 6}deg)`;
-  });
-}
-
-// init
-
-function init() {
-  window.addEventListener('scroll', handleScrolling);
-  pickers.forEach((picker) => {
-    picker.addEventListener('click', handlePick);
-  });
-  back.addEventListener('click', handleBack);
-}
-
-init();
+back.addEventListener('click', handleBack);
